969c342d0febe961afd6c54ca13dbfc54a59cf11
DaneliyaRI <DaneliyaRI@sibedge.com>
2021-06-25 16:57:34 +0700

    Changed env vars names

--- Plv8/config/postgres.js
+++ Plv8/config/postgres.js
@@ -1,11 +1,11 @@
 const connectionString = require('./connection-string').connectionString;
 
 if (connectionString == null || connectionString === '') {
-  const username = process.env.POSTGRES_USER;
-  const password = process.env.POSTGRES_PASSWORD;
-  const host = process.env.POSTGRES_HOST;
-  const port = process.env.POSTGRES_PORT;
-  const dbName = process.env.DB_NAME;
+  const username = process.env.PLV8_POSTGRES_USER;
+  const password = process.env.PLV8_POSTGRES_PASSWORD;
+  const host = process.env.PLV8_POSTGRES_HOST;
+  const port = process.env.PLV8_POSTGRES_PORT;
+  const dbName = process.env.PLV8_DB_NAME;
 
   exports.connStr = `postgresql://${username}:${password}@${host}:${port}/${dbName}`;
 } else {
