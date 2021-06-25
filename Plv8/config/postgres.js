602599446cb053cb2945e5c64aeedc35ab52d097
DaneliyaRI <DaneliyaRI@sibedge.com>
2021-06-25 14:33:44 +0700

    Added connectionString.js import in case you want hardcoded db connection string.

--- Plv8/config/postgres.js
+++ Plv8/config/postgres.js
@@ -1,6 +1,14 @@
-const username = process.env.POSTGRES_USER;
-const password = process.env.POSTGRES_PASSWORD;
-const host = process.env.POSTGRES_HOST;
-const port = process.env.POSTGRES_PORT;
-const dbName = process.env.DB_NAME
-exports.connStr = `postgresql://${username}:${password}@${host}:${port}/${dbName}`;
+const connectionString = require('./connection-string').connectionString;
+
+if (connectionString == null || connectionString === '') {
+  const username = process.env.POSTGRES_USER;
+  const password = process.env.POSTGRES_PASSWORD;
+  const host = process.env.POSTGRES_HOST;
+  const port = process.env.POSTGRES_PORT;
+  const dbName = process.env.DB_NAME;
+
+  exports.connStr = `postgresql://${username}:${password}@${host}:${port}/${dbName}`;
+} else {
+  exports.connStr = connectionString;
+}
+
